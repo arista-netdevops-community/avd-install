4b1c67da31ab261baa95d87f367d1b19056abbfb
Thomas Grimonet <tgrimonet@arista.com>
2020-09-15 09:16:32 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #
 # Purpose: AVD Installation script
-#          Available using https://get.avd.sh
+#          Available using curl -fsSL https://get.avd.sh | sh
 # Author: @titom73
 # Date: 2020-09-14
-# Version: 1
+# Update: 2020-09-15
+# Version: 1.0.1
 # License: Apache 2.0
 # --------------------------------------
 
 # Local Installation Path
-_ROOT_INSTALLATION_DIR="${PWD}/arista-ansible"
+_INSTALLATION_PATH="arista-ansible"
+_ROOT_INSTALLATION_DIR="${PWD}/${_INSTALLATION_PATH}"
 
 # List of Arista Repositories
 _REPO_AVD="https://github.com/aristanetworks/ansible-avd.git"
@@ -23,6 +25,26 @@ _LOCAL_EXAMPLES="${_ROOT_INSTALLATION_DIR}/ansible-avd-cloudvision-demo"
 
 # Folder where Dockerfile and Makefile are located
 _DEV_FOLDER="${_LOCAL_AVD}/development/"
+
+# Print post-installation instructions
+info_installation_done() {
+    echo ""
+    echo "Installtion done."
+    echo ""
+    echo "You can access setup at ${_ROOT_INSTALLATION_DIR} to review all the files"
+    echo "You can login to AVD environment with:"
+    echo "--------------------------------------"
+    echo "   cd ${_ROOT_INSTALLATION_DIR}"
+    echo "   make shell"
+    echo "--------------------------------------"
+    echo ""
+    echo "For complete documentaiton, visit: https://www.avd.sh/docs/installation/setup-environement/"
+    echo ""
+}
+
+##############################################
+# Main content for script
+##############################################
 
 echo "Arista Ansible installation is starting"
 
@@ -38,25 +60,19 @@ if [ ! -d "${_ROOT_INSTALLATION_DIR}" ]; then
     echo "  * creating local installation folder: ${_ROOT_INSTALLATION_DIR}"
     mkdir -p ${_ROOT_INSTALLATION_DIR}
     echo "  * cloning ansible-avd collections to ${_LOCAL_AVD}"
-    git clone ${_REPO_AVD} ${_LOCAL_AVD}
+    git clone ${_REPO_AVD} ${_LOCAL_AVD} > /dev/null 2>&1
     echo "  * cloning ansible-cvp collections to ${_LOCAL_CVP}"
-    git clone ${_REPO_CVP} ${_LOCAL_CVP}
+    git clone ${_REPO_CVP} ${_LOCAL_CVP} > /dev/null 2>&1
     echo "  * cloning netdevops-examples collections to ${_LOCAL_EXAMPLES}"
-    git clone ${_REPO_EXAMPLES} ${_LOCAL_EXAMPLES}
+    git clone ${_REPO_EXAMPLES} ${_LOCAL_EXAMPLES} > /dev/null 2>&1
     if [ -d ${_DEV_FOLDER} ]; then
-        echo "deploying development content to ${_ROOT_INSTALLATION_DIR}"
+        echo "  * deploying development content to ${_ROOT_INSTALLATION_DIR}"
         cp ${_DEV_FOLDER}/Makefile ${_ROOT_INSTALLATION_DIR}
     else
         echo "  ! error: development folder is missing: ${_DEV_FOLDER}"
         exit 1
     fi
-    echo ""
-    echo "Installtion done."
-    echo "You can access setup at ${_ROOT_INSTALLATION_DIR} to review all the files"
-    echo "You can login to AVD environment with:"
-    echo "   cd arista-ipspace"
-    echo "   make shell"
-    echo "For complete documentaiton, visit: https://www.avd.sh/docs/installation/setup-environement/"
+    info_installation_done
 else
     echo "  ! local installation folder already exists - ${_ROOT_INSTALLATION_DIR}"
     exit 1
