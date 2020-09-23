d98f689d2196d3f4b5d610c5f606f48487dbb06c
Thomas Grimonet <tgrimonet@arista.com>
2020-09-23 14:27:22 +0200

    Update install.sh

--- docs/atd/install.sh
+++ docs/atd/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Purpose: AVD Installation script
-#          Available using curl -fsSL https://get.avd.sh/atd/ | sh
+#          Available using curl -fsSL https://get.avd.sh/atd/install.sh | sh
 #          WORK IN PROGRESS
 # Author: @titom73
 # Date: 2020-09-17
@@ -10,6 +10,10 @@
 # License: Apache 2.0
 # --------------------------------------
 
+# Update PATH to match ATD Jumphost setting
+export PATH=$PATH:$HOME/.local/bin/
+export PYTHONPATH=$PYTHONPATH:$HOME/.local/lib/python3.8/site-packages/
+
 # Local Installation Path
 _INSTALLATION_PATH="arista-ansible"
 _ROOT_INSTALLATION_DIR="${PWD}/${_INSTALLATION_PATH}"
@@ -17,7 +21,7 @@ _ROOT_INSTALLATION_DIR="${PWD}/${_INSTALLATION_PATH}"
 # List of Arista Repositories
 _REPO_AVD="https://github.com/aristanetworks/ansible-avd.git"
 _REPO_CVP="https://github.com/aristanetworks/ansible-cvp.git"
-_REPO_EXAMPLES="https://github.com/arista-netdevops-community/ansible-avd-cloudvision-demo"
+_REPO_EXAMPLES="https://github.com/titom73/avd-atd-provisionner"
 
 # Path for local repositories
 _LOCAL_AVD="${_ROOT_INSTALLATION_DIR}/ansible-avd"
@@ -26,7 +30,7 @@ _LOCAL_EXAMPLES="${_ROOT_INSTALLATION_DIR}/ansible-avd-cloudvision-demo"
 
 # BIN Paths
 _PIP_BIN=$(which pip)
-_PIP_OPTIONS="--disable-pip-version-check --quiet --upgrade"
+_PIP_OPTIONS="--disable-pip-version-check --quiet --upgrade --use-feature=2020-resolver"
 _CURL_BIN=$(which curl)
 _CURL_OPTIONS="-fsSL"
 
@@ -45,7 +49,9 @@ info_installation_done() {
     echo "You can access setup at ${_ROOT_INSTALLATION_DIR} to review all the files"
     echo "You can login to AVD environment with:"
     echo "--------------------------------------"
+    echo "   source avd_venv/bin/activate"
     echo "   cd ${_ROOT_INSTALLATION_DIR}"
+    echo "   < start playing with AVD content >"
     echo "--------------------------------------"
     echo ""
     echo "For complete documentaiton, visit: https://www.avd.sh/docs/installation/setup-environement/"
@@ -58,11 +64,24 @@ avd_python_provision() {
     _PYTHON_VERSION=$(python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))')
     echo "  * Python found in version ${_PYTHON_VERSION}"
     echo "    - Standard requirements installation from ${_AVD_REQUIREMENTS_URL}"
-    ${_CURL_BIN} ${_CURL_OPTIONS} ${_AVD_REQUIREMENTS_URL} -o /tmp/requirements.txt
     ${_PIP_BIN} install ${_PIP_OPTIONS} -r /tmp/requirements.txt
     echo "    - Development requirements installation from ${_AVD_DEV_REQUIREMENTS_URL}"
-    ${_CURL_BIN} ${_CURL_OPTIONS} ${_AVD_DEV_REQUIREMENTS_URL} -o /tmp/dev-requirements.txt
     ${_PIP_BIN} install ${_PIP_OPTIONS} -r /tmp/dev-requirements.txt
+}
+
+avd_get_req_files() {
+    ${_CURL_BIN} ${_CURL_OPTIONS} ${_AVD_REQUIREMENTS_URL} -o /tmp/requirements.txt
+    ${_CURL_BIN} ${_CURL_OPTIONS} ${_AVD_DEV_REQUIREMENTS_URL} -o /tmp/dev-requirements.txt
+}
+
+avd_python_provision_venv() {
+    _AVD_VENV="avd_venv"
+    _PYTHON_PATH=$(which python3)
+    virtualenv --clear -q -p ${_PYTHON_PATH} ${PWD}/${_AVD_VENV}
+    echo "    * Standard requirements installation from ${_AVD_REQUIREMENTS_URL}"
+    ${_AVD_VENV}/bin/pip install ${_PIP_OPTIONS} -r /tmp/requirements.txt
+    echo "    * Development requirements installation from ${_AVD_DEV_REQUIREMENTS_URL}"
+    ${_AVD_VENV}/bin/pip install ${_PIP_OPTIONS} -r /tmp/dev-requirements.txt
 }
 
 # user_shell_provisioning() {
@@ -88,7 +107,11 @@ else
 fi
 
 echo "  * Start python requirements installation"
-avd_python_provision
+echo "    * Get requirements files from AVD repository"
+avd_get_req_files
+#avd_python_provision
+echo "    * Provision virtual-env"
+avd_python_provision_venv
 
 echo "  * Deployed AVD environment"
 if [ ! -d "${_ROOT_INSTALLATION_DIR}" ]; then
