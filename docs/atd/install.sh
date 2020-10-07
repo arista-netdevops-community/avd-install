5cfc6a6bbf0cca0092f3ffd561621bf8ff1d61bf
Thomas Grimonet <tgrimonet@arista.com>
2020-10-07 19:20:22 +0200

    Update ATD installation script in v1.0

--- docs/atd/install.sh
+++ docs/atd/install.sh
@@ -2,11 +2,10 @@
 #
 # Purpose: AVD Installation script
 #          Available using curl -fsSL https://get.avd.sh/atd/install.sh | sh
-#          WORK IN PROGRESS
 # Author: @titom73
 # Date: 2020-09-17
-# Update: 2020-09-17
-# Version: 0.0.1
+# Update: 2020-10-07
+# Version: 1.0
 # License: Apache 2.0
 # --------------------------------------
 
@@ -28,71 +27,21 @@ _LOCAL_AVD="${_ROOT_INSTALLATION_DIR}/ansible-avd"
 _LOCAL_CVP="${_ROOT_INSTALLATION_DIR}/ansible-cvp"
 _LOCAL_EXAMPLES="${_ROOT_INSTALLATION_DIR}/atd-avd"
 
-# BIN Paths
-_PIP_BIN=$(which pip3)
-# For SSH jumphost
-#_PIP_OPTIONS="--disable-pip-version-check --quiet --upgrade --use-feature=2020-resolver"
-_PIP_OPTIONS="--disable-pip-version-check --quiet --upgrade"
-_CURL_BIN=$(which curl)
-_CURL_OPTIONS="-fsSL"
-
-# Link to resources to install
-_AVD_REQUIREMENTS_URL="https://raw.githubusercontent.com/aristanetworks/ansible-avd/devel/development/requirements.txt"
-_AVD_DEV_REQUIREMENTS_URL="https://raw.githubusercontent.com/aristanetworks/ansible-avd/devel/development/requirements-dev.txt"
-
-# Folder where Dockerfile and Makefile are located
-_DEV_FOLDER="${_LOCAL_AVD}/development/"
-
 # Print post-installation instructions
 info_installation_done() {
     echo ""
     echo "Installtion done."
     echo ""
-    echo "You can access setup at ${_ROOT_INSTALLATION_DIR} to review all the files"
+    echo "You can access setup at ${_ROOT_INSTALLATION_DIR} to review all the files: collections and playbooks"
     echo "You can login to AVD environment with:"
     echo "--------------------------------------"
-    echo "   source avd_venv/bin/activate"
-    echo "   cd ${_ROOT_INSTALLATION_DIR}"
+    echo "   cd ${_LOCAL_EXAMPLES}"
     echo "   < start playing with AVD content >"
     echo "--------------------------------------"
     echo ""
-    echo "For complete documentaiton, visit: https://www.avd.sh/docs/installation/setup-environement/"
+    echo "For complete documentaiton, visit: ttps://github.com/arista-netdevops-community/atd-avd"
     echo ""
 }
-
-# Function to bootstrap AVD Python requirements from AVD repository
-# WARNING: Python version is not enforced
-avd_python_provision() {
-    _PYTHON_VERSION=$(python -c 'import sys; version=sys.version_info[:3]; print("{0}.{1}.{2}".format(*version))')
-    echo "  * Python found in version ${_PYTHON_VERSION}"
-    echo "    - Standard requirements installation from ${_AVD_REQUIREMENTS_URL}"
-    ${_PIP_BIN} install ${_PIP_OPTIONS} -r /tmp/requirements.txt
-    echo "    - Development requirements installation from ${_AVD_DEV_REQUIREMENTS_URL}"
-    ${_PIP_BIN} install ${_PIP_OPTIONS} -r /tmp/dev-requirements.txt
-}
-
-avd_get_req_files() {
-    ${_CURL_BIN} ${_CURL_OPTIONS} ${_AVD_REQUIREMENTS_URL} -o /tmp/requirements.txt
-    ${_CURL_BIN} ${_CURL_OPTIONS} ${_AVD_DEV_REQUIREMENTS_URL} -o /tmp/dev-requirements.txt
-}
-
-avd_python_provision_venv() {
-    _AVD_VENV="avd_venv"
-    _PYTHON_PATH=$(which python3)
-    ${_PIP_BIN} install ${_PIP_OPTIONS} --user virtualenv
-    virtualenv --clear -q -p ${_PYTHON_PATH} ${PWD}/${_AVD_VENV}
-    echo "    * Standard requirements installation from ${_AVD_REQUIREMENTS_URL}"
-    ${_AVD_VENV}/bin/pip install ${_PIP_OPTIONS} -r /tmp/requirements.txt
-    echo "    * Development requirements installation from ${_AVD_DEV_REQUIREMENTS_URL}"
-    ${_AVD_VENV}/bin/pip install ${_PIP_OPTIONS} -r /tmp/dev-requirements.txt
-}
-
-# user_shell_provisioning() {
-#     ${_CURL_BIN} ${_CURL_OPTIONS} https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
-#     echo 'plugins=(ansible common-aliases safe-paste git jsontools history git-extras)' >> $HOME/.zshrc
-#     echo 'eval `ssh-agent -s`' >> $HOME/.zshrc
-#     echo 'export TERM=xterm' >>  $HOME/.zshrc
-# }
 
 
 ##############################################
@@ -108,13 +57,6 @@ else
     echo "  ! git is not installed, installation aborted"
     exit 1
 fi
-
-# echo "  * Start python requirements installation"
-# echo "    * Get requirements files from AVD repository"
-# avd_get_req_files
-#avd_python_provision
-# echo "    * Provision virtual-env"
-# avd_python_provision_venv
 
 echo "  * Deployed AVD environment"
 if [ ! -d "${_ROOT_INSTALLATION_DIR}" ]; then
