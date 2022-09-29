efd78cb191b8365e57aba6ca9ebe6df0f7036be0
Carl Buchmann <carl.buchmann@arista.com>
2022-09-29 12:20:00 -0400

    Bump: AVD and CVP collection versions

--- docs/atd/install.sh
+++ docs/atd/install.sh
@@ -36,8 +36,8 @@ _LOCAL_EXAMPLES="${_ROOT_INSTALLATION_DIR}/atd-avd"
 # _AVD_VERSION=$(curl -L -I -s -o /dev/null -w %{url_effective} https://github.com/aristanetworks/ansible-avd/releases/latest | cut -d "/" -f 8)
 # _CVP_VERSION=$(curl -L -I -s -o /dev/null -w %{url_effective} https://github.com/aristanetworks/ansible-cvp/releases/latest | cut -d "/" -f 8)
 # Static configuration
-_AVD_VERSION=v3.1.0
-_CVP_VERSION=v3.2.0
+_AVD_VERSION=v3.7.0
+_CVP_VERSION=v3.4.0
 
 # Print post-installation instructions
 info_installation_done() {
